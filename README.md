2f88a6c62ffd15f5054f350f912915fba1bbb812
Tim Hutt <tdhutt@gmail.com>
2023-05-27 20:44:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Expr
+
+Expr is a very simple integer expression calculator. Its grammar is as follows:
+
+identifier := [A-Za-z_] [A-Za-z_0-9]*
+variable := identifier
+literal := decimal_literal | hex_literal | binary_literal | octal_literal
+binary_op := '*' | '-' | '/' | '%' | '+' | '==' | '<' ...
+unary_op := '!' | '~' | identifier
+brackets := '(' atom ')'
+unary := unary_op atom
+atom := brackets | unary | literal
+expr := atom ( binary_op atom )*
+
+Very standard with two exceptions:
+
+1. There is no unary + or -.
+2. Function calls always take exactly 1 argument and do not require brackets (simialr to functional languages with currying).
+
+The only supported types are `bool` and `u64`. There is no type coersion.
+
+Operator meaning and precedence is the same as in Go (or Rust with the exception of `~`).
+
+There is no undefined behaviour. Overflows wrap. Large shifts result in 0.
---
+++ src/main.rs
@@ -0,0 +1,205 @@
+// Implement custom Result type.
+
+use std::fmt;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum ExprError {
+    SyntaxError(String),
+    TypeError(String),
+}
+
+impl fmt::Display for ExprError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
+
+type Result<T> = std::result::Result<T, ExprError>;
+
+#[derive(PartialEq, Eq, Debug)]
+enum Val {
+    Int(u64),
+    Bool(bool),
+}
+
+enum Ast {
+    Plus(Box<Ast>, Box<Ast>),
+    Minus(Box<Ast>, Box<Ast>),
+    Mul(Box<Ast>, Box<Ast>),
+    Div(Box<Ast>, Box<Ast>),
+    Eq(Box<Ast>, Box<Ast>),
+    NotEq(Box<Ast>, Box<Ast>),
+    Not(Box<Ast>),
+    BitNot(Box<Ast>),
+    Literal(Val),
+}
+
+impl Ast {
+    pub fn evaluate(&self) -> Result<Val> {
+        match self {
+            Ast::Plus(a, b) => match (a.evaluate()?, b.evaluate()?) {
+                (Val::Int(a), Val::Int(b)) => Ok(Val::Int(a.wrapping_add(b))), // TODO: Should be a separate operator like `@+`
+                _ => Err(ExprError::TypeError("+ operands must be ints".to_string())),
+            }
+            Ast::Minus(a, b) => match (a.evaluate()?, b.evaluate()?) {
+                (Val::Int(a), Val::Int(b)) => Ok(Val::Int(a.wrapping_sub(b))), // TODO: Should be a separate operator like `@-`
+                _ => Err(ExprError::TypeError("- operands must be ints".to_string())),
+            }
+            _ => todo!()
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum BinOp {
+    Plus,
+    Minus,
+    Mul,
+    Div,
+    Eq,
+    NotEq,
+    LogicalAnd,
+    BitAnd,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum UnOp {
+    Not,
+    BitNot,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum Token {
+    Literal(Val),
+    BinOp(BinOp),
+    UnOp(UnOp),
+    Identifier(String),
+    LeftBracket,
+    RightBracket,
+}
+
+#[derive(PartialEq, Eq)]
+enum State {
+    None,
+    Int,
+    Identifier,
+}
+
+fn tokenise(input: &str) -> Result<Vec<Token>> {
+    // Split on whitespace and word boundaries.
+
+    let mut state = State::None;
+    let mut tokens = Vec::new();
+
+    // For literals and identifiers.
+    let mut word = String::new();
+
+    let mut iter = input.chars().peekable();
+
+    while let Some(c) = iter.next() {
+        // Push current literal/identifier if it has ended.
+        if !matches!(c, '0'..='9' | 'A'..='Z' | 'a'..='z' | '_') {
+            if !word.is_empty() {
+                match state {
+                    State::Int => {
+                        tokens.push(Token::Literal(Val::Int(word.parse().map_err(|_| {
+                            ExprError::SyntaxError(format!("invalid integer: {}", word))
+                        })?)));
+                    }
+                    State::Identifier => {
+                        tokens.push(Token::Identifier(word));
+                    }
+                    _ => unreachable!(),
+                }
+                word = String::new();
+                state = State::None;
+            }
+        }
+
+        match c {
+            '0'..='9' => {
+                if state == State::None {
+                    state = State::Int;
+                }
+                word.push(c);
+            }
+            'A'..='Z' | 'a'..='z' | '_' => {
+                if state == State::None {
+                    state = State::Identifier;
+                }
+                word.push(c);
+            }
+            '+' => {
+                tokens.push(Token::BinOp(BinOp::Plus));
+            }
+            '&' => {
+                if iter.peek() == Some(&'&') {
+                    iter.next();
+                    tokens.push(Token::BinOp(BinOp::LogicalAnd));
+                } else {
+                    tokens.push(Token::BinOp(BinOp::BitAnd));
+                }
+            }
+            ' ' | '\t' | '\n' => {}
+            _ => {
+                return Err(ExprError::SyntaxError(format!(
+                    "unexpected character: {}",
+                    c
+                )))
+            }
+        }
+    }
+
+    if !word.is_empty() {
+        match state {
+            State::Int => {
+                tokens.push(Token::Literal(Val::Int(word.parse().map_err(|_| {
+                    ExprError::SyntaxError(format!("invalid integer: {}", word))
+                })?)));
+            }
+            State::Identifier => {
+                tokens.push(Token::Identifier(word));
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    Ok(tokens)
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_tokeniser() {
+        assert_eq!(
+            tokenise("1 + 2"),
+            Ok(vec![
+                Token::Literal(Val::Int(1)),
+                Token::BinOp(BinOp::Plus),
+                Token::Literal(Val::Int(2)),
+            ])
+        );
+
+        assert_eq!(
+            tokenise("1+2*foo(bar)"),
+            Ok(vec![
+                Token::Literal(Val::Int(1)),
+                Token::BinOp(BinOp::Plus),
+                Token::Literal(Val::Int(2)),
+                Token::BinOp(BinOp::Mul),
+                Token::Identifier("foo".to_string()),
+                Token::LeftBracket,
+                Token::Identifier("bar".to_string()),
+                Token::RightBracket,
+            ])
+        );
+
+
+    }
+}
