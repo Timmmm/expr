b2b1e9d8acb4cbe4cf2ad2a6b223c19e1b1439ae
Tim Hutt <tdhutt@gmail.com>
2023-06-10 14:01:23 +0100

    Add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -49,4 +49,45 @@ mod test {
         let result = expr.evaluate(&SimpleContext {});
         assert_eq!(result, Ok(Val::Int(1 + 2 * 3 + (4 + 1) * (2 + 1) + 1)));
     }
+
+    #[test]
+    fn test_comprehensive() {
+        // Very exhaustive test of all operators, with lots of brackets and
+        // function calls.
+        let tokens = tokenise(
+            "x + y * 3 + (4 + 1) * inc(2) + 1 == 1 + 2 * 3 + (4 + 1) * inc(2) + 1 \
+             && x + y * 3 + (4 + 1) * inc(2) + 1 != 1 + 2 * 3 + (4 + 1) * inc(2) + 1 \
+             || x + y * 3 + (4 + 1) * inc(2) + 1 < 1 + 2 * 3 + (4 + 1) * inc(2) + 1 \
+             && x + y * 3 + (4 + 1) * inc(2) + 1 <= 1 + 2 * 3 + (4 + 1) * inc(2) + 1 \
+             || x + y * 3 + (4 + 1) * inc(2) + 1 > 1 + 2 * 3 + (4 + 1) * inc(2) + 1 \
+             && x + y * 3 + (4 + 1) * inc(2) + 1 >= 1 + 2 * 3 + (4 + 1) * inc(2) + 1",
+        ).unwrap();
+        let expr = parse(tokens).unwrap();
+        let result = expr.evaluate(&SimpleContext {});
+        assert_eq!(result, Ok(Val::Bool(false)));
+    }
+
+    // // Generate a random valid expression.
+    // fn random_expr() -> Expr {
+    //     use rand::distributions::{Distribution, Standard};
+    //     use rand::Rng;
+
+    //     let mut rng = rand::thread_rng();
+    //     let mut stack = Vec::new();
+    //     let mut expr = Expr::Literal(Val::Int(rng.gen()));
+    //     let mut depth = 0;
+    //     while depth > 0 || rng.gen::<bool>() {
+    //         if depth > 0 && rng.gen::<bool>() {
+    //             depth -= 1;
+    //             expr = stack.pop().unwrap();
+    //         } else {
+    //             depth += 1;
+    //             let op = rng.gen::<BinOp>();
+    //             let new_expr = Expr::BinOp(Box::new(expr), Box::new(Expr::Literal(Val::Int(rng.gen()))), op);
+    //             stack.push(expr);
+    //             expr = new_expr;
+    //         }
+    //     }
+    //     expr
+    // }
 }
